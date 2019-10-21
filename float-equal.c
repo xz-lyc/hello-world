91402f63bd2c1232db89bc76841408ce7202ea85
xz-lyc <lyc2940014277@163.com>
2019-10-21 20:58:51 +0800

    float-equal.c add commit

--- float-equal.c
+++ float-equal.c
@@ -6,10 +6,9 @@ int main()
     float m = 1.78;
     float x = 1.779999, y = 2;
     float z = m / y, z1 = x / y;
-    if (z == z1) {
+    if (z-z1<0.00001)
         z = (m + x) / y;
-    }
-    printf("Z is %f\n", z);
-    printf("Z1 is %f\n", z1);
+    printf("Z is %.5f\n", z);
+    printf("Z1 is %.5f\n", z1);
     return 0;
 }
